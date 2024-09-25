509ded9553693d3760dc13c3d34e9669f06d23f4
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-09-24 20:14:04 -0600

    Add files via upload

---
+++ tarea 2.2/tarea 2.2.py
@@ -0,0 +1,21 @@
+
+#Arzab Diaz April 3w
+
+#esta linea dice mi nombre
+
+#Programa que solicite al usuario ingresar la cantidad de kilómetros recorridos por una motocicleta y la cantidad de litros de combustible que consumió durante ese recorrido. Mostrar el consumo de combustible por kilómetro.
+
+#esta linea me dice lo que ara el programa
+
+
+kilometros_recorridos=float(input("ingresa la cantidad de kilometros recorridos:"))
+
+litros_consumidos=float(input("ingresa la cantidad de litros de combustible consumidos:"))
+
+#esta linea calcula la cantidad de combustible por killometro 
+
+consumo_por_kilometro=litros_consumidos/kilometros_recorridos
+
+#esta linea muestra la cantidad del consumo del combustible por kilometro
+
+print("el consumo de conbustible por kilometro  es :",consumo_por_kilometro,"litros/kilometro")
